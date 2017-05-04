9bd327159b0cbb5a57bbcb813b87bbe1852f7010
Erica Wright and Sarah Leahy <sarah@>
2017-05-04 12:02:12 -0700

    add area geofield for leaflet map

--- sites/all/themes/customstyle/js/scripts.js
+++ sites/all/themes/customstyle/js/scripts.js
@@ -1,16 +1,12 @@
 console.log('JS file links');
 
-
 var s = Snap("#mysvg");
+var bigCircle = s.circle(150, 150, 100);
+
 var text = s.text(200,120, 'Snap Animation Test');
 text.attr({
     fill: "#0000ff",
     "font-size": "30px",
     "font-weight": "bold"
 });
-var polygon = canvas.rect(100, 200, 500, 300);
-
-
-jQuery(document).ready(function($) {
-    $("#content").append(polygon);
-});
+// var polygon = canvas.rect(100, 200, 500, 300);
